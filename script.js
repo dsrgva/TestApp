97091a296913b8677bb305d48e074982a2cb56ca
Darya Sergeeva <dsgva98@gmail.com>
2024-10-20 21:38:02 +0300

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 'use strict';
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+const personMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?','');
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+
+console.log(personMovieDB);
